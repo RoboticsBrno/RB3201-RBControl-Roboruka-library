61b8cda7ec8492dbab6b21ff465f4e5d35837c61
Vojtěch Boček <vojtech.bocek@avast.com>
2022-05-06 10:04:19 +0200

    Update RBProtocol and RBGridUi

--- src/_librk_wifi.cpp
+++ src/_librk_wifi.cpp
@@ -185,7 +185,7 @@ void WiFi::setupBle(const rkConfig& cfg, const Config& wifiCfg) {
         name->setValue(cfg.name);
         m_chars.push_back(name);
 
-        auto ip = rb::WiFi::getIp().addr;
+        auto ip = rb::WiFi::getIp();
         m_ip_char = service->createCharacteristic(WIFI_IP_UUID,
             BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
         m_ip_char->setValue(ip);
@@ -250,7 +250,7 @@ void WiFi::scheduleIpUpdateLocked() {
 
 bool WiFi::updateIpChar() {
     auto ip = rb::WiFi::getIp();
-    if (ip.addr == 0) {
+    if (ip == 0) {
         return true;
     }
 
@@ -258,7 +258,7 @@ bool WiFi::updateIpChar() {
     if (m_ip_char == nullptr)
         return false;
 
-    m_ip_char->setValue(ip.addr);
+    m_ip_char->setValue(ip);
     m_ip_char->notify();
     m_ip_update_running = false;
     return false;
